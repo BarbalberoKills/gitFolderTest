a9091a408abfca49ea42c9803454d0664817624c
Andrea <andreapinchera.1989@gmail.com>
2025-10-25 18:39:26 +0200

    feat: add create nested files and folder

--- autocreate/main.go
+++ autocreate/main.go
@@ -5,8 +5,14 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"slices"
 )
+
+type Node struct {
+	Name     string
+	Indent   int
+	Path     string
+	Children []*Node
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -15,40 +21,103 @@ func main() {
 		os.Exit(1)
 	}
 
-	content := ""
-	if len(os.Args) == 3 {
-		content = os.Args[2]
+	// if len(os.Args) == 3 {
+	// 	content = os.Args[2]
+	// }
+
+	list, path := getFile(os.Args[1])
+
+	// filtered := []string{}
+	// ignore := []string{"---", "sorting-spec: |-", "  sortspec"}
+	// for i := 0; i < len(list); i++ {
+	// 	if !slices.Contains(ignore, list[i]) {
+	// 		filtered = append(filtered, list[i][2:])
+	// 	}
+	// }
+
+	root := Node{
+		Name:   "root",
+		Indent: -1,
+		Path:   path,
 	}
 
-	file := getFile(os.Args[1])
-	defer file.Close()
+	stack := []*Node{}
+
+	stack = append(stack, &root)
+
+	for i := 0; i < len(list); i++ {
+		indent := 0
+		var name string
+		for _, c := range list[i] {
+			if string(c) != " " {
+				name = list[i][indent:]
+				break
+			}
+			indent += 1
+		}
+
+		for indent <= stack[len(stack)-1].Indent {
+			stack = stack[:len(stack)-1]
+		}
+
+		parent := stack[len(stack)-1]
+
+		newNode := &Node{
+			Name:   name,
+			Indent: indent,
+			Path:   parent.Path + "/" + parent.Name,
+		}
+		if parent.Indent == -1 {
+			newNode.Path = parent.Path
+		}
+
+		parent.Children = append(parent.Children, newNode)
+
+		stack = append(stack, newNode)
+	}
+
+	for _, child := range root.Children {
+		child.Maker()
+	}
+}
+
+func (n *Node) Maker() {
+	if len(n.Children) == 0 {
+		touch(n.Path, n.Name, "md", "")
+	} else {
+		makeFolder(n.Path, n.Name)
+	}
+	for _, child := range n.Children {
+		child.Maker()
+	}
+}
+
+func getFile(passedFile string) ([]string, string) {
+	file, err := os.Open(passedFile)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
 	list := []string{}
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		list = append(list, scanner.Text())
 	}
+	defer file.Close()
 
-	filtered := []string{}
-	ignore := []string{"---", "sorting-spec: |-", "  sortspec"}
-	for i := 0; i < len(list); i++ {
-		if !slices.Contains(ignore, list[i]) {
-			filtered = append(filtered, list[i][2:])
-		}
-	}
-
-	for _, note := range filtered {
-		touch(filepath.Dir(file.Name()), note, "md", content)
-	}
+	return list, filepath.Dir(file.Name())
 }
 
-func getFile(passedFile string) *os.File {
-	file, err := os.Open(passedFile)
+func makeFolder(path, name string) {
+	folderName := path + "/" + name
+	err := os.MkdirAll(folderName, 0755)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	return file
+	fmt.Println("")
+	fmt.Println("Created folder: " + folderName)
 }
 
 func touch(path, name, ext, content string) {
@@ -58,5 +127,10 @@ func touch(path, name, ext, content string) {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	fmt.Println("Created file: " + fileName + "with content" + content)
+	if content != "" {
+		fmt.Println("Created file: " + fileName + " with content" + content)
+	} else {
+		fmt.Println("")
+		fmt.Println("Created file: " + fileName)
+	}
 }

a027b5a47f468820ae48c9ac39cc2f57f704c25b
Andrea <andreapinchera.1989@gmail.com>
2025-10-27 18:44:33 +0100

    feat:extrac getParms logic

--- autocreate/main.go
+++ autocreate/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -15,25 +16,7 @@ type Node struct {
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Missing arguments")
-		fmt.Println("Usage: bulklist-md <filename> [optional-content]")
-		os.Exit(1)
-	}
-
-	// if len(os.Args) == 3 {
-	// 	content = os.Args[2]
-	// }
-
-	list, path := getFile(os.Args[1])
-
-	// filtered := []string{}
-	// ignore := []string{"---", "sorting-spec: |-", "  sortspec"}
-	// for i := 0; i < len(list); i++ {
-	// 	if !slices.Contains(ignore, list[i]) {
-	// 		filtered = append(filtered, list[i][2:])
-	// 	}
-	// }
+	list, path, content, fileType, dry := getParams()
 
 	root := Node{
 		Name:   "root",
@@ -41,20 +24,10 @@ func main() {
 		Path:   path,
 	}
 
-	stack := []*Node{}
+	stack := []*Node{&root}
 
-	stack = append(stack, &root)
-
-	for i := 0; i < len(list); i++ {
-		indent := 0
-		var name string
-		for _, c := range list[i] {
-			if string(c) != " " {
-				name = list[i][indent:]
-				break
-			}
-			indent += 1
-		}
+	for _, line := range list {
+		name, indent := indentCount(line)
 
 		for indent <= stack[len(stack)-1].Indent {
 			stack = stack[:len(stack)-1]
@@ -77,18 +50,66 @@ func main() {
 	}
 
 	for _, child := range root.Children {
-		child.Maker()
+		child.Maker(content, fileType, dry)
 	}
 }
 
-func (n *Node) Maker() {
+func getParams() ([]string, string, string, string, bool) {
+	dry := flag.Bool("dry", false, "Simulation mode")
+	content := flag.String("content", "", "Content to write")
+	fileType := flag.String("ext", "", "File extension")
+	flag.Parse()
+
+	os.Args = os.Args[1:]
+
+	if *dry {
+		os.Args = os.Args[1:]
+	}
+	if *content != "" {
+		os.Args = os.Args[1:]
+	}
+	if *fileType != "" {
+		os.Args = os.Args[1:]
+		*fileType = "." + *fileType
+	}
+
+	if len(os.Args) < 1 {
+		fmt.Println("Missing arguments")
+		fmt.Println("Usage: treebuild [-dry] [-content] [-ext] <filename>")
+		os.Exit(1)
+	} else if len(os.Args) > 1 {
+		fmt.Println("Too many arguments")
+		fmt.Println("Usage: treebuild [-dry] [-content] [-ext] <filename>")
+		os.Exit(1)
+	}
+
+	list, path := getFile(os.Args[len(os.Args)-1])
+
+	return list, path, *content, *fileType, *dry
+
+}
+
+func indentCount(line string) (string, int) {
+	indent := 0
+	var name string
+	for _, c := range line {
+		if string(c) != " " {
+			name = line[indent:]
+			break
+		}
+		indent += 1
+	}
+	return name, indent
+}
+
+func (n *Node) Maker(content, fileType string, dry bool) {
 	if len(n.Children) == 0 {
-		touch(n.Path, n.Name, "md", "")
+		touch(n.Path, n.Name, fileType, content, dry)
 	} else {
-		makeFolder(n.Path, n.Name)
+		makeFolder(n.Path, n.Name, dry)
 	}
 	for _, child := range n.Children {
-		child.Maker()
+		child.Maker(content, fileType, dry)
 	}
 }
 
@@ -109,23 +130,27 @@ func getFile(passedFile string) ([]string, string) {
 	return list, filepath.Dir(file.Name())
 }
 
-func makeFolder(path, name string) {
+func makeFolder(path, name string, dry bool) {
 	folderName := path + "/" + name
-	err := os.MkdirAll(folderName, 0755)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	if !dry {
+		err := os.MkdirAll(folderName, 0755)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 	}
 	fmt.Println("")
 	fmt.Println("Created folder: " + folderName)
 }
 
-func touch(path, name, ext, content string) {
-	fileName := path + "/" + name + "." + ext
-	err := os.WriteFile(fileName, []byte(content), 0755)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+func touch(path, name, ext, content string, dry bool) {
+	fileName := path + "/" + name + ext
+	if !dry {
+		err := os.WriteFile(fileName, []byte(content), 0755)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 	}
 	if content != "" {
 		fmt.Println("Created file: " + fileName + " with content" + content)
